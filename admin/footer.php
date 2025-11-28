9bf3e49a442bbe50340fc703b97ae6dd5342f222
nabilajanifasofiana09-dotcom <nabilajanifasofiana09@gmail.com>
2025-11-28 09:17:17 +0700

    Initial commit

---
+++ admin/footer.php
@@ -0,0 +1,31 @@
+<footer class="bg-dark text-white pt-4 pb-2">
+  <div class="container">
+    <div class="row">
+
+      <!-- Info Laundry -->
+      <div class="col-md-6 mb-3">
+        <h4><i class="fa fa-shopping-basket"></i> Laundry</h4>
+        <p>Layanan laundry cepat, bersih, dan wangi.</p>
+        <p>
+          <i class="glyphicon glyphicon-map-marker"></i> Kendal, Jawa Tengah <br>
+          <i class="glyphicon glyphicon-earphone"></i> 0812-3456-7890 <br>
+          <i class="glyphicon glyphicon-envelope"></i> info@laundry.com
+        </p>
+      </div>
+
+      <!-- Sosial Media -->
+      <div class="col-md-6 mb-3 text-center">
+        <h5>Ikuti Kami</h5>
+        <a href="https://facebook.com" class="text-white mx-2"><i class="fa fa-facebook fa-2x"></i></a>
+        <a href="https://instagram.com" class="text-white mx-2"><i class="fa fa-instagram fa-2x"></i></a>
+        <a href="https://twitter.com" class="text-white mx-2"><i class="fa fa-twitter fa-2x"></i></a>
+        <a href="https://wa.me/6281234567890" class="text-white mx-2"><i class="fa fa-whatsapp fa-2x"></i></a>
+        <a href="https://tiktok.com" class="text-white mx-2"><i class="fa fa-music fa-2x"></i></a>
+      </div>
+
+    </div>
+
+    <hr class="bg-white">
+    <p class="text-center mb-0">&copy; 2025 Sistem Informasi Laundry | All Rights Reserved</p>
+  </div>
+</footer>
